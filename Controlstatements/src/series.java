a4d59665aa83cd4e27c23b512c4bb6213d32b4b9
shivaniv13 <shivani18csu206@ncuindia.edu>
2019-01-15 11:57:31 +0530

    added

--- Controlstatements/src/series.java
+++ Controlstatements/src/series.java
@@ -1,30 +1,35 @@
 //Author:Shivani Vashishta
 //Version:1.0.0
 // Purpose: Java program to find sum of series 
-// 1 + 1/x + x^2/2 + x^3/3 + ....+ x^n/n
+// 1 + 1/x + x^2/2! + x^3/3! + ....+ x^n/n!
 
-import java.util.*; 
-  
+import java.util.Scanner;
 class series
-{ 
-      
-static double sum(int x, int n) 
-{ 
-    double i, total = 1.0; 
-    for (i = 1; i <= n; i++) 
-        total = total +  
-                (Math.pow(x, i) / i); 
-  
-    return total; 
-} 
-  
-public static void main(String[] args) 
-{ Scanner input = new Scanner(System.in);
-
-    int x = input.nextInt(); 
-    int n = input.nextInt(); 
-
-    System.out.printf("%.2f", sum(x, n)); 
-} 
+{
+    public static int factser(int index)
+    {
+        int f = 1, a;
+        for(a= 1; a<= index; a++)
+        {
+             f= f*a;
+        }
+        return f;
+    }
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner(System.in);
+        int a , no , x;
+        double frac, sum=1;
+        System.out.println("Enter the number of terms i.e n");
+        no= in.nextInt();
+        System.out.println("Enter the value of x ");
+        x = in.nextInt();
+        for(a = 1; a <= no; a ++)
+        {
+            frac= Math.pow(x, a)/factser(a);
+            sum = sum + frac;
+        }
+        System.out.println("The sum for the series 1 + 1/x + x^2/2! + x^3/3! + ....+ x^n/n! is  "+sum);
+    }
 } 
   
